9b00d41c32d48dfb2d6f5281fe3625645cb69fe4
caseyanderson <casey.thomas.anderson@gmail.com>
2013-10-10 15:25:53 -0700

    adding other sensors into this hacky test file

--- arduino/sensor_station_test_basic.ino
+++ arduino/sensor_station_test_basic.ino
@@ -2,6 +2,13 @@
 Sensor Station Test
  */
 
+ int sensorValue1;
+ int sensorValue2;
+ int sensorValue3;
+ int sensorValue4;
+ int sensorValue5;
+ int sensorValue6;
+
 // super rough way to test sensor station
 void setup() {
   // initialize serial communication at 9600 bits per second:
@@ -9,9 +16,11 @@ void setup() {
 }
 
 void loop() {
-  int sensorValue1 = analogRead(A0);
-  int sensorValue2 = analogRead(A1);
-  int sensorValue3 = analogRead(A2);
+  sensorValue1 = analogRead(A0);
+  sensorValue2 = analogRead(A1);
+  sensorValue3 = analogRead(A2);
+  sensorValue4 = analogRead(A3);
+  sensorValue5 = analogRead(A4); 
   Serial.print( "pot1 = " );
   Serial.print(sensorValue1);
   Serial.print("\t");
@@ -20,6 +29,15 @@ void loop() {
   Serial.print("\t");
   Serial.print( "slider = " );
   Serial.print( sensorValue3 );
+  Serial.print("\t");
+  Serial.print( "photo = " );
+  Serial.print( sensorValue4 );
+  Serial.print("\t");
+  Serial.print( "bend = " );
+  Serial.print( sensorValue5;
+  Serial.print("\t");
+  Serial.print( "force = " );
+  Serial.print( sensorValue6 );
   Serial.println("");
   delay(1);
 }
