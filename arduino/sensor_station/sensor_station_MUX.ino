47c4ae1a0c2d56623b358baa2c854dd0e3022af4
caseyanderson <casey.thomas.anderson@gmail.com>
2015-07-29 12:46:48 -0700

    there is now a sensor station folder, moved all sensor station stuff there

---
+++ arduino/sensor_station/sensor_station_MUX.ino
@@ -0,0 +1,47 @@
+/*
+Sensor Station plus MUX
+
+ * codeexample for useing a 4051 * analog multiplexer / demultiplexer
+ * by david c. and tomek n.* for k3 / malmï¿½ hï¿½gskola
+ *
+ * edited by Ross R.
+ */
+
+int r0 = 0;      //value of select pin at the 4051 (s0)
+int r1 = 0;      //value of select pin at the 4051 (s1)
+int r2 = 0;      //value of select pin at the 4051 (s2)
+int which = 0;   //which y pin we are selecting
+int val;
+
+void setup() {
+pinMode(A0, INPUT);
+  pinMode(2, OUTPUT);    // s0
+  pinMode(3, OUTPUT);    // s1
+  pinMode(4, OUTPUT);    // s2
+}
+
+void loop () {
+
+  which = ((analogRead(A0)) % 8);             // scales potentiometer to 0 - 7, chooses what bit to send MUX, reads at A0
+
+  // select the bit
+  r0 = bitRead(which, 0);
+  r1 = bitRead(which, 1);
+  r2 = bitRead(which, 2);    // use this with arduino 0013 (and newer versions)
+
+  digitalWrite(2, r0);
+  digitalWrite(3, r1);
+  digitalWrite(4, r2);
+
+  //Either read or write the multiplexed pin here
+
+  val = analogRead(A3);     // MUX ouputs here
+
+  Serial.print("sensor number ");
+  Serial.print(which);
+  Serial.print('\t');
+  Serial.print("val is ");
+  Serial.println(val);
+
+  delay(2);
+}
