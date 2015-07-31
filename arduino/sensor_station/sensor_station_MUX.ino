b63d3914e5445610eb702c6d2bf657fdaf132217
caseyanderson <casey.thomas.anderson@gmail.com>
2015-07-30 17:01:52 -0700

    quick test prior to adding second 4051 to check renaming and whatnot

--- arduino/sensor_station/sensor_station_MUX.ino
+++ arduino/sensor_station/sensor_station_MUX.ino
@@ -1,23 +1,39 @@
 /*
 Sensor Station plus MUX
+Casey Anderson, 2015
 
- * codeexample for useing a 4051 * analog multiplexer / demultiplexer
- * by david c. and tomek n.* for k3 / malmï¿½ hï¿½gskola
- *
- * edited by Ross R.
+Based on this help doc: http://playground.arduino.cc/Learning/4051
+
  */
 
-int r0 = 0;      //value of select pin at the 4051 (s0)
-int r1 = 0;      //value of select pin at the 4051 (s1)
-int r2 = 0;      //value of select pin at the 4051 (s2)
-int which = 0;   //which y pin we are selecting
-int val, sensorValue;
+int r0 = 0;
+int r1 = 0;
+int r2 = 0;
+
+int  r3 = 0;
+int  r4 = 0;
+int  r5 = 0;
+
+int mux1, sensorValue1, which1;
+int mux2, sensorValue2, which2;
+
 
 void setup() {
   pinMode(A0, INPUT);       // mux1 input
+  pinMode(A1, INPUT);       // mux2 input
+
+  pinMode(A2, INPUT);       // mux1 sensor selector input
+  pinMode(A3, INPUT);       // mux2 sensor selector input
+
+  //ino to mux 1 (changes what input shows up at 4051 output, attached to A0)
   pinMode(2, OUTPUT);    // s0
   pinMode(3, OUTPUT);    // s1
   pinMode(4, OUTPUT);    // s2
+
+  //ino to mux 2 (changes what input shows up at 4051 output, attached to A1)
+  pinMode(5, OUTPUT);    // s0
+  pinMode(6, OUTPUT);    // s1
+  pinMode(7, OUTPUT);    // s2
 
   Serial.begin (115200);
 }
@@ -25,28 +41,26 @@ void setup() {
 void loop () {
 
   //  which = (7);              // just for testing
-  sensorValue = analogRead(A2);
-  which = map(sensorValue, 0, 1023, 0, 7);
+  sensorValue1 = analogRead(A2);
+  which1 = map(sensorValue1, 0, 1023, 0, 7);
 
   // select the bit (MUX1)
-  r0 = bitRead(which, 0);
-  r1 = bitRead(which, 1);
-  r2 = bitRead(which, 2);
+  r0 = bitRead(which1, 0);
+  r1 = bitRead(which1, 1);
+  r2 = bitRead(which1, 2);
 
   // write the bit (MUX1)
   digitalWrite(2, r0);
   digitalWrite(3, r1);
   digitalWrite(4, r2);
 
-  //Either read or write the multiplexed pin here
-  // refactor, this should be digitalWrite with millis or something
-  val = analogRead(A0);     // MUX1 ouputs here
+  mux1 = analogRead(A0);     // MUX1 ouputs here
 
   Serial.print("sensor number ");
-  Serial.print(which);
+  Serial.print(which1);
   Serial.print('\t');
   Serial.print("val is ");
-  Serial.println(val);
+  Serial.println(mux1);
 
   delay(2);
 }
